1ee3029f30ac56ef147b737ef20e26cc60e66468
Enriquer97 <enriqueser997@gmail.com>
2024-03-07 11:32:57 +0100

    Sonido sol

--- Assets/Scripts/Sound.cs
+++ Assets/Scripts/Sound.cs
@@ -7,21 +7,41 @@ public class Sound : MonoBehaviour
 
     public AudioClip move;
     public AudioClip run;
+    public AudioClip HurtSound;
+    public GameObject Hurt;
     public AudioSource soundSource;
+    public AudioSource soundSource2;
     public GameObject steps;
+    public GameObject Radiacion;
+    public Vector3 PlayerPosition;
     public bool walk;
     // Start is called before the first frame update
     void Start()
     {
         steps.SetActive(false);
         walk = true;
+        Radiacion = GameObject.Find("Radiacion");
+        soundSource2.clip = HurtSound;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (Input.GetKeyDown("w") || Input.GetKeyDown("a") || Input.GetKeyDown("s") || Input.GetKeyDown("d"))
+        PlayerPosition = Radiacion.transform.position;
+        Vector3 dir = Radiacion.transform.position - transform.position;
+        RaycastHit rayHit;
+        if (Physics.Raycast(transform.position, dir, out rayHit, 2000000f))
+        {
+            if (rayHit.collider.gameObject.name == "Radiacion")
+            {
+                OnRayHit();
+            }
+            else
+            {
+                OnNotRayHit();
+            }
+        }
+            if (Input.GetKeyDown("w") || Input.GetKeyDown("a") || Input.GetKeyDown("s") || Input.GetKeyDown("d"))
         {
             soundSource.clip = move;
             steps.SetActive(true);
@@ -30,6 +50,15 @@ public class Sound : MonoBehaviour
         {
             steps.SetActive(false);
 
+        }
+        void OnRayHit()
+        {
+            Hurt.SetActive(true);
+
+        }
+        void OnNotRayHit()
+        {
+            Hurt.SetActive(false);
         }
 
         /*
